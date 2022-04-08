7142f350f638d54d7e578e6bcd610c02b36bb8e5
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2022-04-08 10:38:07 -0700

    set up tests for util functions

---
+++ src/tests/Utils.test.js
@@ -0,0 +1,47 @@
+const {arraysEqual, arrayMatchType, parseList} = require("../utils")
+
+
+describe("Testing arraysEqual", () => {
+
+    test("Should return true if all elements are same type and same value", () => {
+
+    })
+
+    test("Should return false if only types are the same", () => {
+
+    })
+
+    test("Should return false if arrays are different sizes", () => {
+
+    })
+})
+
+describe("Testing arrayMatchType", () => {
+
+    test("Should return true if all elements in array are of same type", () => {
+
+    })
+
+    test("Should return false if any one of the elements is of different type", () => {
+
+    })
+})
+
+describe("Testing parseList", () => {
+
+    test("Should return new position after parsing", () => {
+
+    })
+
+    test("Should return a list of all parseResults", () => {
+
+    })
+
+    test("Should return empty if nothing is parsed", () => {
+
+    })
+
+    test("Should return empty if wrong parseFunction is passed", () => {
+        
+    })
+})
